d7e7e70fd8fad6eb625d91c856b35643c3e3113f
Mohamadreza Hoseinpour <mrz.hsp@gmail.com>
2019-10-25 15:33:23 +0200

    Fixing Question 1.a and 1.b. Designed the dirty compuation for Question 2.a.

--- Adv R Closing Assignment.R
+++ Adv R Closing Assignment.R
@@ -7,162 +7,199 @@
 # Initial Setup -----------------------------------------------------------
 ## Requirements
 library(tidyverse)
+
 load("stocks.RData")
 
 ## Getting a sense of data
 start_date <- as.Date("1990-01-01")
 end_date <- as.Date("2015-12-31")
-retail_stocks <- stocks %>% filter(date >= start_date, date <= end_date) %>% 
-  select(date, TGT, WMT) %>% mutate(ratio = TGT/WMT)
+retail_stocks <- stocks %>%
+  filter(date >= start_date, date <= end_date) %>%
+  select(date, TGT, WMT) %>%
+  mutate(ratio = TGT / WMT)
 mean_ratio_retail_stocks <- mean(retail_stocks$ratio)
 sd_ratio_retail_stocks <- sd(retail_stocks$ratio)
 
+k <- 1.25
 ggplot(retail_stocks, aes(x = date, y = ratio)) +
   geom_line() +
   geom_hline(aes(yintercept = mean_ratio_retail_stocks), color = "blue") +
-  geom_hline(aes(yintercept = mean_ratio_retail_stocks + sd_ratio_retail_stocks * k), color = "red") +
-  geom_hline(aes(yintercept = mean_ratio_retail_stocks - sd_ratio_retail_stocks * k), color = "red")
+  geom_hline(aes(yintercept = mean_ratio_retail_stocks + 
+                   sd_ratio_retail_stocks * k), color = "red") +
+  geom_hline(aes(yintercept = mean_ratio_retail_stocks - 
+                   sd_ratio_retail_stocks * k), color = "red")
 
 # Question 1.a) -----------------------------------------------------------
 
 #' Title: Find Next Position
-#'        Which finds the opening and closing indices of the first new position 
+#'        Which finds the opening and closing indices of the first new position
 #'        on or after a given starting day.
 #' @param ratio The daily price ratio for the two stocks (a numeric vector).
 #' @param starting_from Index of the first day on which the next position could
-#'                      be opened (default value of 1).
+#'                      be opened (default value = 1).
 #' @param k The value of "k" for calculating the decision boundaries (m - k * s)
-#'          and (m + k * s) (default value of 1).
-#' @param m The estimated mean ratio m in the formula for the boundaries 
-#'          (default value equal to the mean of "ratio").
+#'          and (m + k * s) (default value = 1).
+#' @param m The estimated mean ratio m in the formula for the boundaries
+#'          (default value = the mean of "ratio").
 #' @param s The standard deviation s of the price ratio, used in the formula for
-#'          the boundaries (default value equal to the standard deviation of 
-#'          "ratio").
-#'
+#'          the boundaries (default value = the standard deviation of "ratio").
 #' @return An integer vector of length two containing the indices of "ratio"
 #'         where the next position should be opened and closed. In case no
 #'         position is found, it returns a length zero integer vector.
-#' @export 
+#' @export
 #'
-#' @examples 
+#' @examples
 find_next_position <- function(ratio, starting_from = 1, k,
                                m = mean(ratio), s = sd(ratio)) {
+  #' Turn the input into a dataframe and make sure that starting_from is
+  #'   stored as a number when I want the next positions in the next iterations.
+  data <- as.data.frame(ratio)
+  starting_from <- as.numeric(starting_from)
+  #' A condition to check whether the "k" is too big or not. If it is, then an
+  #'   empty integer vector.
   if ((m - k * s) < min(data) & (m + k * s) > max(data)) {
     return(integer())
   } else {
-    data <- as.data.frame(ratio)
-    open <- which(data[starting_from:nrow(data), ] < (m - k * s) | 
-                    data[starting_from:nrow(data), ] > (m + k * s))[1] +
+    #' The opening positions will be called and stored based on the criteria.
+    open <- which(data[starting_from:nrow(data), ] < (m - k * s) |
+      data[starting_from:nrow(data), ] > (m + k * s))[1] +
       (starting_from - 1)
-    open_num <- data[open, ]
-    close <- if (open_num < m) {
+    #' The closing posiiton will be called based on the criterai that the open
+    #'   was below or above the mean.
+    close <- if (data[open, ] < m) {
       which(data[open:nrow(data), ] >= m)[1] + open - 1
     } else {
       which(data[open:nrow(data), ] <= m)[1] + open - 1
     }
+    #' This is the condition for checking whether the function has reached the
+    #'   end of the dataframe.
     if (is.na(close)) {
       close <- nrow(data)
     }
-    c(open = open, close = close)
+    c("open" = open, "close" = close)
   }
 }
 
 ## Testing the function
-pos_1 <- find_next_position(ratio = retail_stocks$ratio, 
-                            k = 1.25)
+pos_1 <- find_next_position(
+  ratio = retail_stocks$ratio,
+  k = 1.25
+)
 pos_1
 retail_stocks[pos_1, ]
-pos_2 <- find_next_position(ratio = retail_stocks$ratio,
-                            starting_from = pos_1[2], k = 1.25)
+pos_2 <- find_next_position(
+  ratio = retail_stocks$ratio,
+  starting_from = pos_1[2], k = 1.25
+)
 pos_2
 retail_stocks[pos_2, ]
-pos_3 <- find_next_position(ratio = retail_stocks$ratio,
-                            starting_from = pos_2[2], k = 1.25)
+pos_3 <- find_next_position(
+  ratio = retail_stocks$ratio,
+  starting_from = pos_2[2], k = 1.25
+)
 pos_3
 retail_stocks[pos_3, ]
-pos_4 <- find_next_position(ratio = retail_stocks$ratio,
-                            starting_from = pos_3[2], k = 1.25)
+pos_4 <- find_next_position(
+  ratio = retail_stocks$ratio,
+  starting_from = pos_3[2], k = 1.25
+)
 pos_4
+retail_stocks[pos_4, ]
 pos_5 <- find_next_position(ratio = retail_stocks$ratio, k = 3.25)
 pos_5
 
 # Question 1.b) ------------------------------------------------------------
-ratio <- retail_stocks$ratio
-data <- as.data.frame(ratio)
-starting_from <- 1
-k <- 1.25
-m <- mean_ratio_retail_stocks
-s <- sd_ratio_retail_stocks
-condition1 <- m - k * s
-condition2 <- m + k * s
-min_data <- min(data)
-max_data <- max(data)
-i <- 1
 
-find_all_positions <- function (ratio, k = 1, m, s) {
-  if ((m - k * s) < min(data) & (m + k * s) > max(data)) {
-    return(list())
-    } else {
-      starting_from <- 1
-      i <- 1
-      positions <- vector("list", i)
-      positions[[i]] <- find_next_position(ratio = retail_stocks$ratio, 
-                                           starting_from, k = 1.25)
-      names(positions) <- paste("position", 1, sep = "_")
-      while (map(positions, 2)[[i]] < nrow(data)) {
-        starting_from <- map(positions, 2)[[i]]
-        i <- i + 1
-        positions[[i]] <- find_next_position(ratio = retail_stocks$ratio, 
-                                               starting_from, k = 1.25)
-        names(positions) <- paste("position", 1:i, sep = "_")
-      }
-    }
-}
-
-
-
-
+#' Title Find All Positons
+#'       Which finds all the opening and closing indices.
+#'Which finds the opening and closing indices of the first new position
+#'        on or after a given starting day.
+#' @param ratio The daily price ratio for the two stocks (a numeric vector).
+#' @param k The value of "k" for calculating the decision boundaries (m - k * s)
+#'          and (m + k * s) (default value = 1).
+#' @param m The estimated mean ratio m in the formula for the boundaries
+#'          (default value = the mean of "ratio").
+#' @param s The standard deviation s of the price ratio, used in the formula for
+#'          the boundaries (default value = the standard deviation of "ratio").
+#' @return A list of the indices of all positions.
+#' @export
+#'
+#' @examples
+find_all_positions <- function(ratio, k, m = mean(ratio),
+                               s = sd(ratio)) {
+  #' Turn the input into a dataframe.
+  data <- as.data.frame(ratio)
+  #' A condition to check whether there are positions. If there is none, then it
+  #'   returns a length-zero list.
   if ((m - k * s) < min(data) & (m + k * s) > max(data)) {
     return(list())
   } else {
+    #' The initial values needed for the loop are assigned.
     starting_from <- 1
     i <- 1
     positions <- vector("list", i)
-    positions[[i]] <- find_next_position(ratio = retail_stocks$ratio, 
-                                         starting_from, k = 1.25)
-    names(positions) <- paste("position", 1, sep = "_")
+    #' The function from Q1.a is used.
+    positions[[i]] <- find_next_position(ratio, starting_from, k)
+    #' This is for naming the elements of the list.
+    names(positions) <- paste("positions", 1, sep = "_")
     while (map(positions, 2)[[i]] < nrow(data)) {
+      #' The loop starts from the first closing position.
       starting_from <- map(positions, 2)[[i]]
       i <- i + 1
-      positions[[i]] <- find_next_position(ratio = retail_stocks$ratio, 
-                                           starting_from, k = 1.25)
-      names(positions) <- paste("position", 1:i, sep = "_")
+      positions[[i]] <- find_next_position(ratio, starting_from, k)
+      names(positions) <- paste("positions", 1:i, sep = "_")
     }
   }
-
-if (length(all_positions) > 0) {
-  names(all_positions) <- paste("position", 1:(i - 1), sep = "_")
+  return(positions)
 }
-all_positions
+positions <- find_all_positions(ratio = retail_stocks$ratio, k = 1.25)
+
+# Question 2.a) -------------------------------------------------------------------
+
+position_profit <- function(position, stock_a, stock_b,
+                            m = mean(stock_a / stock_b), p = 0.01) {
+  
 }
 
-positions <- vector("list", 1)
-positions[[1]] <- find_next_position(ratio = retail_stocks$ratio, 
-                                         starting_from = 1, k = 1.25)
-positions[[2]] <- find_next_position(ratio = retail_stocks$ratio, 
-                                         starting_from = map(positions, 2)[[1]],
-                                         k = 1.25)
-positions[[3]] <- find_next_position(ratio = retail_stocks$ratio, 
-                                     starting_from = map(positions, 2)[[2]],
-                                     k = 1.25)
-positions[[4]] <- find_next_position(ratio = retail_stocks$ratio, 
-                                     starting_from = map(positions, 2)[[3]],
-                                     k = 1.25)
-positions[[5]] <- find_next_position(ratio = retail_stocks$ratio, 
-                                     starting_from = map(positions, 2)[[4]],
-                                     k = 1.25)
+data <- as.data.frame(retail_stocks$ratio)
+x <- c(map(positions, 1)[[1]], map(positions, 2)[[1]])
+b <- retail_stocks[x, ]
+m <- mean(retail_stocks$ratio)
+p <- 0.01
+open_unit <- as.numeric(1 / b[1, 2])
+close_unit <- as.numeric(1 / b[1, 3])
+revenue_a <- open_unit * as.numeric(b[2, 2])
+revenue_b <- close_unit * as.numeric(b[2, 3])
+if (data[x[1], ] < m) {
+  profit_stock_a <- revenue_a - 1
+  profit_stock_b <- 1 - revenue_b
+} else {
+  profit_stock_a <- -(revenue_a - 1)
+  profit_stock_b <- -(1 - revenue_b)
+}
+cost <- -(2 * p + (revenue_a + revenue_b) * p)
+profit <- profit_stock_a + profit_stock_b + cost
+output <- c(profit_stock_a, profit_stock_b, cost, profit)
+output
 
-# Dirty work for Q1.a)
+profit_stock_a <- revenue_a - 1
+profit_stock_b <- 1 - revenue_b
+cost <- -(2 * p + (revenue_a + revenue_b) * p)
+profit <- stock_a + stock_b + cost
+output <- c(profit_stock_a, profit_stock_b, cost, profit)
+output
+
+if (data[x[1], ] < m) {
+  revenue_a <- open_unit * as.numeric(b[2, 2])
+  revenue_b <- close_unit * as.numeric(b[2, 3])
+  profit_stock_a <- revenue_a - 1
+  profit_stock_b <- 1 - revenue_b
+  cost <- -(2 * p + (revenue_a + revenue_b) * p)
+  profit <- stock_a + stock_b + cost
+}
+
+
+# Dirty Work for Q1.a) ----------------------------------------------------
 starting_from <- 1743
 k <- 4
 m <- mean_ratio_retail_stocks
@@ -173,8 +210,8 @@ min_data <- min(data)
 max_data <- max(data)
 
 ## Calcualtion
-first_open <- which(retail_stocks$ratio < m - k * s | 
-                retail_stocks$ratio > m + k * s)[1]
+first_open <- which(retail_stocks$ratio < m - k * s |
+                      retail_stocks$ratio > m + k * s)[1]
 open <- retail_stocks$ratio[first_open]
 data_new <- slice(retail_stocks, first_open:n())
 first_close <- if (open < m) {
@@ -184,8 +221,8 @@ first_close <- if (open < m) {
 }
 # goooz
 data <- as.data.frame(ratio)
-first_open <- which(data[starting_from:nrow(data), ] < m - k * s | 
-                      data[starting_from:nrow(data), ] > m + k * s)[1] + 
+first_open <- which(data[starting_from:nrow(data), ] < m - k * s |
+                      data[starting_from:nrow(data), ] > m + k * s)[1] +
   (starting_from - 1)
 open <- ratio[first_open]
 first_close <- if (open < m) {
@@ -204,11 +241,64 @@ x <- nrow(retail_stocks)
 pos_1 <- c(first_open, first_close)
 retail_stocks[pos_1, ]
 
-
+# Dirty Work for Q1.b) ----------------------------------------------------
+data <- as.data.frame(ratio)
 if ((m - k * s) < min(data) & (m + k * s) > max(data)) {
-  result <- 1
+  return(list())
 } else {
-  result <- 0
+  starting_from <- 1
+  i <- 1
+  positions <- vector("list", i)
+  positions[[i]] <- find_next_position(
+    ratio = retail_stocks$ratio,
+    starting_from, k = 1.25
+  )
+  names(positions) <- paste("position", 1, sep = "_")
+  while (map(positions, 2)[[i]] < nrow(data)) {
+    starting_from <- map(positions, 2)[[i]]
+    i <- i + 1
+    positions[[i]] <- find_next_position(
+      ratio = retail_stocks$ratio,
+      starting_from, k = 1.25
+    )
+    names(positions) <- paste("position", 1:i, sep = "_")
+  }
 }
 
-(-0.2707*-0.2707)+(-0.8809*-0.8809)+(0.2526*0.2526)+(-0.2948*-0.2948)
+positions <- vector("list", 1)
+positions[[1]] <- find_next_position(
+  ratio = retail_stocks$ratio,
+  starting_from = 1, k = 1.25
+)
+positions[[2]] <- find_next_position(
+  ratio = retail_stocks$ratio,
+  starting_from = map(positions, 2)[[1]],
+  k = 1.25
+)
+positions[[3]] <- find_next_position(
+  ratio = retail_stocks$ratio,
+  starting_from = map(positions, 2)[[2]],
+  k = 1.25
+)
+positions[[4]] <- find_next_position(
+  ratio = retail_stocks$ratio,
+  starting_from = map(positions, 2)[[3]],
+  k = 1.25
+)
+positions[[5]] <- find_next_position(
+  ratio = retail_stocks$ratio,
+  starting_from = map(positions, 2)[[4]],
+  k = 1.25
+)
+
+ratio <- retail_stocks$ratio
+data <- as.data.frame(ratio)
+starting_from <- 1
+k <- 1.25
+m <- mean_ratio_retail_stocks
+s <- sd_ratio_retail_stocks
+condition1 <- m - k * s
+condition2 <- m + k * s
+min_data <- min(data)
+max_data <- max(data)
+i <- 1
